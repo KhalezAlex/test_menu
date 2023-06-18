f00e1cf2910a4606f61c48ba9e0b88dcee75f011
KhalezAlex <klozevitz@yandex.ru>
2023-06-18 15:03:45 +0300

    Initial commit

---
+++ src/main/java/org/klozevitz/test_menu/TestMenuApplication.java
@@ -0,0 +1,13 @@
+package org.klozevitz.test_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/klozevitz/test_menu/TestMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.klozevitz.test_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
